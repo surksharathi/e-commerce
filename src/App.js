619b3edcedde56701080f8dd961c7f560dd5d250
surksha rathi <surksha8@gmail.com>
2019-11-21 15:38:47 +0530

    login page is added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homePage.component";
 import ShopPage from "../src/pages/shopPage/shope.component";
-import Header from "./components/header/header.component.jsx";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
