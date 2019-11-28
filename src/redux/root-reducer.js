60eb4443256d2b639d57c993ba2555f2bbce17ad
surksha rathi <surksha8@gmail.com>
2019-11-28 13:44:05 +0530

    cart data presist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,19 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+ 
 
-export default combineReducers({
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['cart']
+}
+
+const rootReducer= combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(rootPersistConfig,rootReducer)
