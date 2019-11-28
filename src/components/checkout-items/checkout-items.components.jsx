2c319bb7fe43cf2162b0be60ece6c7af79e0a902
surksha rathi <surksha8@gmail.com>
2019-11-28 11:25:07 +0530

    item revoved feature in checkout page implemented

--- src/components/checkout-items/checkout-items.components.jsx
+++ src/components/checkout-items/checkout-items.components.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./checkout-items.styles.scss";
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import { connect } from "react-redux";
+// import { dispatch } from "rxjs/internal/observable/range";
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img alt="item" src={imageUrl}></img>
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl}></img>
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
-  </div>
-);
+  );
+};
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+});
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
