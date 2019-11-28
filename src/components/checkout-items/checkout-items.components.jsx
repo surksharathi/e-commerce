7159242f049c487b6256910049194eb49f415e97
surksha rathi <surksha8@gmail.com>
2019-11-28 11:48:57 +0530

    item add and remove feature implemented in checkout page

--- src/components/checkout-items/checkout-items.components.jsx
+++ src/components/checkout-items/checkout-items.components.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./checkout-items.styles.scss";
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.actions";
 import { connect } from "react-redux";
 // import { dispatch } from "rxjs/internal/observable/range";
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
@@ -12,8 +16,16 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img alt="item" src={imageUrl}></img>
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
-      <span className="price">{price}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -22,7 +34,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
   );
 };
 const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item))
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
